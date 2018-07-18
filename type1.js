558353993e4e6e713aeb666ba45335a6e4714ec3
gerardog17 <pacoescobedo17@gmail.com>
2018-07-17 21:46:25 -0500

    first commit

---
+++ type1.js
@@ -0,0 +1,20 @@
+var nombre;
+nombre = 'Gerardo';
+var edad;
+edad: 28;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+//JS
+var spiderman = {
+    nombre: 'Peter Parker',
+    poderes: ['telaraña', 'escalar', 'sentido araña']
+};
+interface;
+_super.Heroe;
+{
+    nombre: string;
+    poderes: string[];
+}
+var suma = function (a, b) { return (a + b); };
---
+++ type1.ts
@@ -0,0 +1,23 @@
+let nombre:string;
+    nombre = 'Gerardo';
+let edad: number;
+    edad: 28;
+
+const PERSONAJE = {
+    nombre:nombre,
+    edad:edad
+};
+
+//JS
+var spiderman ={
+    nombre : 'Peter Parker',
+    poderes : ['telaraña', 'escalar', 'sentido araña']
+}
+
+interface super Heroe{
+    nombre:string;
+    poderes:string[];
+}
+
+let suma = (a:number, b:number) => (a+b);
+
